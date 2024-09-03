a0df8073a572825138a53c9fb3f5a33f20076157
manu989898 <apkmanu1@gmail.com>
2024-09-03 12:58:03 +0200

    ddd

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
